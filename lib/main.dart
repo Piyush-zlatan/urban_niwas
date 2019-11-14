98222a0cd43092304f4333c9da7fc8bc0532a02d
Piyush-zlatan <12piyush10@gmail.com>
2019-11-14 16:03:55 +0530

    Inital commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:urban_niwas/login.dart';
+import 'package:urban_niwas/register.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter login UI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyRegisterPage(title: 'Flutter Login'),
+    );
+  }
+}
